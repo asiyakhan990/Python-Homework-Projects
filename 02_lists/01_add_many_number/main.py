529aa1db2ae66c4f657a84a23218ed88422b6e2e
Asiya Khan <160250124+asiyakhan990@users.noreply.github.com>
2025-03-26 01:02:55 +0500

    first Commit

---
+++ 02_lists/01_add_many_number/main.py
@@ -0,0 +1,10 @@
+def add_many_numbers(numbers: list[int]) -> int:
+    
+    return sum(numbers)  
+
+def main():
+    numbers = [1, 2, 3, 4, 5]  
+    print(add_many_numbers(numbers))  
+
+if __name__ == "__main__":
+    main()
